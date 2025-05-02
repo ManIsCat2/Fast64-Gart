f0a7e2c72172c442772e76d536477c341406d4cb
TheGreatMario64 <109289447+thegreatmario64@users.noreply.github.com>
2025-05-02 16:47:51 -0500

    F3DEX2E isn't only in SM64COOPDX

--- fast64_internal/f3d/f3d_enums.py
+++ fast64_internal/f3d/f3d_enums.py
@@ -390,7 +390,7 @@ enumF3D = [
         "Variant of F3DEX2 family using vertex rejection instead of clipping",
         5,
     ),
-    ("F3DEX2E", "F3DEX2E", "Microcode used in sm64coopdx", 11),
+    ("F3DEX2E", "F3DEX2E", "Microcode used in most SM64 PC-Ports", 11),
     ("F3DEX3", "F3DEX3", "Custom microcode by Sauraen", 6),
     ("", "Homebrew", "", 8),
     ("RDPQ", "RDPQ", "Base libdragon microcode", 9),
